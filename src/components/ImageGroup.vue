dc5cadf657d8a0568a6144dd56ab9cd78506e8d2
Lucas Boyer <lrboyer@clemson.edu>
2023-11-26 23:46:48 -0500

    added a faggot

---
+++ src/components/ImageGroup.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="group" :style="{ backgroundColor: groupColor }">
+        <h1>{{ groupName }}</h1>
+        <div class="images">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    props: {
+        groupName: String,
+        groupColor: String,
+    },
+};
+</script>
+  
+<style scoped>
+.group {
+    margin-bottom: 20px;
+    padding: 20px;
+    border-radius: 8px;
+}
+
+.images {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+    grid-auto-flow: dense;
+    grid-gap: 10px;
+    background-color: burlywood;
+}
+</style>
+  
